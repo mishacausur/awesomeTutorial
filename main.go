aefbd54d8d8274e43c957c61b729f9bbe8280176
Misha Causur <mishacausur@mail.ru>
2022-01-09 23:27:08 +0500

    task 3 done

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var i, a, b, c, d, e, f uint
+	var i, sum int
 	fmt.Scan(&i)
-	a = i / 100000
-	b = (i % 100000) / 10000
-	c = (i % 10000) / 1000
-	d = (i % 1000) / 100
-	e = (i % 100) / 10
-	f = (i % 10)
-	switch {
-	case (a + b + c) == (d + e + f):
-		fmt.Print("YES")
-	default:
-		fmt.Print("NO")
+	for i != 0 {
+		var y int
+		fmt.Scan(&y)
+		if (y > 10 && y < 99) && (y%8 == 0) {
+			sum += y
+		}
+		i--
 	}
-
+	fmt.Print(sum)
 }
