5e43e84dd127818fd2bf9783a38223a7afd29570
Misha Causur <mishacausur@mail.ru>
2022-01-09 22:19:18 +0500

    initial commit

--- scratch.go
+++ scratch.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var d int
-	fmt.Scan(&d)
-	var hours = d / 30
-	var minutes = (d % 30) * 2
-	fmt.Print("It is ", hours, " hours, ", minutes, " minutes")
+	var i uint
+	fmt.Scan(&i)
+	switch {
+	case i > 0:
+		fmt.Print("Число положительное")
+	case i < 0:
+		fmt.Print("Число отрицательное")
+	default:
+		fmt.Print("Ноль")
+	}
 }
