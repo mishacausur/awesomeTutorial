dcb41fe2dbc71b50e8bf31ab84e4ca95d845938d
Misha Causur <mishacausur@mail.ru>
2022-01-09 22:57:00 +0500

    initial commit

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	var i int
+	var i, a, b, c, d, e, f uint
 	fmt.Scan(&i)
+	a = i / 100000
+	b = (i % 100000) / 10000
+	c = (i % 10000) / 1000
+	d = (i % 1000) / 100
+	e = (i % 100) / 10
+	f = (i % 10)
 	switch {
-	case i > 0:
-		fmt.Print("Число положительное")
-	case i < 0:
-		fmt.Print("Число отрицательное")
+	case (a + b + c) == (d + e + f):
+		fmt.Print("YES")
 	default:
-		fmt.Print("Ноль")
+		fmt.Print("NO")
 	}
+
 }
