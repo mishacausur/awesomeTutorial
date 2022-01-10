f70f7aa9543a603fa19a6b550087b2e94993125f
Misha Causur <mishacausur@mail.ru>
2022-01-10 17:21:03 +0500

    task 4 done

--- main.go
+++ main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var i, sum int
-	fmt.Scan(&i)
-	for i != 0 {
-		var y int
-		fmt.Scan(&y)
-		if (y > 10 && y < 99) && (y%8 == 0) {
-			sum += y
+	var x, y, z int
+	fmt.Scan(&x, &y)
+	for a := 1000; x/a >= 0; a = a % 10 {
+		if x/a == 0 {
+			continue
 		}
-		i--
+		z = (x / a) % 10
+		for e := 1000; y/e >= 0; e = e % 10 {
+			if z == (y/e)%10 {
+				fmt.Print(z, " ")
+			}
+			if e == 1 {
+				break
+			}
+		}
+		if a == 1 {
+			break
+		}
 	}
-	fmt.Print(sum)
 }
