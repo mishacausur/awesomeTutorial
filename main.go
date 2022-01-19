20f31979c35de94b26157cfbe76c47fa55a12ff5
Misha Causur <mishacausur@mail.ru>
2022-01-19 23:10:51 +0500

    task 4 done

--- main.go
+++ main.go
@@ -1,27 +1,43 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var x, y, z int
-	fmt.Scan(&x, &y)
-	for a := 1000; x/a >= 0; a = a / 10 {
-		if x/a == 0 {
-			continue
-		}
-		z = (x / a) % 10
-		for e := 1000; y/e >= 0; e = e / 10 {
-			if z == (y/e)%10 {
-				fmt.Print(z, " ")
-			}
-			if e == 1 {
-				break
-			}
-		}
-		if a == 1 {
-			break
-		}
+	var workArray = [10]uint8{}
+	var i uint8
+	var index = 0
+
+	for fmt.Scan(&i); index <= len(workArray)-1; fmt.Scan(&i) {
+		workArray[index] = i
+		fmt.Println(workArray)
+		index++
 	}
+
+	//var indexed = [6]uint{}
+	var indexes = [6]uint8{}
+	var num uint8
+	var index1 = 0
+	for fmt.Scan(&num); index1 <= len(indexes)-1; fmt.Scan(&num) {
+		fmt.Println(indexes)
+		indexes[index1] = num
+		index1++
+		fmt.Println(indexes)
+	}
+
+	var a, b, c, d, e, f uint8
+	a = workArray[indexes[0]]
+	b = workArray[indexes[1]]
+	c = workArray[indexes[2]]
+	d = workArray[indexes[3]]
+	e = workArray[indexes[4]]
+	f = workArray[indexes[5]]
+
+	workArray[indexes[0]] = b
+	workArray[indexes[1]] = a
+	workArray[indexes[2]] = d
+	workArray[indexes[3]] = c
+	workArray[indexes[4]] = f
+	workArray[indexes[5]] = e
+
+	fmt.Println(workArray)
 }
