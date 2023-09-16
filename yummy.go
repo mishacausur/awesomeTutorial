ee9cf3d669e6d2419401990a723d1d438299f0e7
Misha Causur <mishacausur@mail.ru>
2023-09-16 21:17:54 +0500

    task 4.1 done

--- yummy.go
+++ yummy.go
@@ -3,24 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var number int
-	var counter = 0
-	fmt.Scanln(&number)
-
-	for i := 2; counter < 10; i++ {
-		var res = fib(i)
-		if res < number {
-			continue
-		}
-		fmt.Println(res)
-		counter++
+	for i := 0; i < 10; i++ {
+		fmt.Println(i + 1)
 	}
-}
-
-func fib(n int) int {
-
-	if n < 2 {
-		return n
-	}
-	return fib(n-1) + fib(n-2)
 }
