0a63bdba033c7eb62c4d6cb9103c3cf68eec7cda
Misha Causur <mishacausur@mail.ru>
2023-09-17 21:19:55 +0500

    task 4.7 done

--- yummy.go
+++ yummy.go
@@ -4,11 +4,15 @@ import "fmt"
 
 func main() {
 	var number int
+	var res = 0
 	fmt.Scanln(&number)
 
 	for i := 1; i <= number; i++ {
-		if i%3 == 0 {
-			fmt.Println(i)
+		if (i%3 == 0) || (i%5 == 0) {
+
+			continue
 		}
+		res += i
 	}
+	fmt.Println(res)
 }
