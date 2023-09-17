8f25ef1e3dfeab4be1a418192d888ebf5b4d5756
Misha Causur <mishacausur@mail.ru>
2023-09-17 21:19:22 +0500

    task 4.5 done

--- yummy.go
+++ yummy.go
@@ -4,10 +4,17 @@ import "fmt"
 
 func main() {
 	var number int
-	var result = 1
+	var result = 0
 	fmt.Scanln(&number)
+	var text = "Некорректный ввод"
 	for i := 1; i <= number; i++ {
-		result = result * i
+		if number < 0 {
+			break
+		}
+		if i%2 != 0 {
+			result = result + i
+			text = fmt.Sprintln(result)
+		}
 	}
-	fmt.Println(result)
+	fmt.Println(text)
 }
