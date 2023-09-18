3e8d2a9e037654745e5b22d58e77c882d90f0f05
Misha Causur <mishacausur@mail.ru>
2023-09-18 22:27:30 +0500

    task 5.2 done

--- yummy.go
+++ yummy.go
@@ -1,17 +1,8 @@
 package main
 
-import "fmt"
-
-func main() {
-	IsPowerOfTwoRecursive(18)
-}
-
-func IsPowerOfTwoRecursive(N int) {
-	if N == 1 {
-		fmt.Print("YES")
-	} else if N > 1 && N%2 == 0 {
-		IsPowerOfTwoRecursive(N / 2)
-	} else {
-		fmt.Print("NO")
+func SumDigitsRecursive(n int) int {
+	if n < 10 {
+		return n
 	}
+	return n%10 + SumDigitsRecursive(n/10)
 }
