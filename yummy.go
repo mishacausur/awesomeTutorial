d3b8f86156b3b983a1685e2f70932524fe5da00e
Misha Causur <mishacausur@mail.ru>
2023-09-24 22:58:01 +0500

    task 7.3 done

--- yummy.go
+++ yummy.go
@@ -3,10 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	output := StringLength("evrb")
+	output := SumOfValuesInMap(map[int]int{10: 37, 3: 19})
 	fmt.Print(output)
 }
 
-func StringLength(input string) int {
-	return len(input)
+func SumOfValuesInMap(m map[int]int) int {
+	var result int
+
+	for key := range m {
+		result += m[key]
+	}
+	return result
 }
