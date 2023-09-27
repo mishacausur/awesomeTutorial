18d3f6447084aa51198ac3141cf96c220a1f3241
Misha Causur <mishacausur@mail.ru>
2023-09-27 22:12:33 +0500

    task 9.3 done

--- main.go
+++ main.go
@@ -1,66 +1,37 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-)
-
-type Jumper interface {
-	WhereAmI() int      // выводит текущее положение кузнечика на линейке
-	Jump() (int, error) // кузнечик прыгает к зерну. Выводит новое положение кузнечика, или ошибку, если он уже ест зерно
-}
-
-type Grasshopper struct {
-	position int
-	target   int
-}
-
-func (g *Grasshopper) WhereAmI() int {
-	return g.position
-}
-
-func (g *Grasshopper) Jump() (int, error) {
-	if g.position == g.target {
-		return g.position, errors.New("error")
-	}
-
-	maxJump := 5
-	distanceToTarget := g.target - g.position
-
-	if distanceToTarget <= maxJump {
-		g.position = g.target
-		return g.target, nil
-	}
-	jumpDistance := 1
-
-	if jumpDistance < 1 || jumpDistance > maxJump {
-		return g.position, errors.New("error")
-	}
-
-	g.position += jumpDistance
-	return g.position, nil
-}
-
-func PlaceJumper(place, target int) Jumper {
-	return &Grasshopper{
-		position: place,
-		target:   target,
-	}
-}
-
-const (
-	place  = 0
-	target = 3
+	"strings"
 )
 
 func main() {
-	g := PlaceJumper(place, target)
-	fmt.Println(g.WhereAmI())
+
+	var count int
+	fmt.Scan(&count)
+
+	people := make([]string, count)
+
+	for i := 0; i < count; i++ {
+		fmt.Scan(&people[i])
+	}
+
 	for {
-		currPlace, err := g.Jump()
-		if err != nil {
+		var prefix string
+		if _, error := fmt.Scan(&prefix); error != nil {
 			break
 		}
-		fmt.Println(currPlace)
+
+		result := false
+		for _, name := range people {
+			if strings.HasPrefix(name, prefix) {
+				fmt.Println(name)
+				result = true
+				break
+			}
+		}
+		if !result {
+			fmt.Println("Не найдено")
+		}
 	}
 }
