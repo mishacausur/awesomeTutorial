6aad8c5f196ea5b5545d9c6d7c7b89d7d72c94b3
Misha Causur <mishacausur@mail.ru>
2023-09-27 22:17:04 +0500

    jsn 0.2

--- main.go
+++ main.go
@@ -3,32 +3,18 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
-	"os"
 )
 
 type Person struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name string `json:"name"`
+	Age  int    `json:"age"`
 }
 
 func main() {
-	file, err := os.Open("data.json")
+	person := Person{Name: "John", Age: 30}
+	jsonBytes, err := json.Marshal(person)
 	if err != nil {
-		fmt.Println("Ошибка при открытии файла:", err)
-		return
+		panic(err)
 	}
-	defer file.Close()
-
-	byteValue, _ := io.ReadAll(file)
-
-	var person Person
-	err = json.Unmarshal(byteValue, &person)
-	if err != nil {
-		fmt.Println("Ошибка при декодировании JSON:", err)
-		return
-	}
-
-	fmt.Println("Имя:", person.Name)
-	fmt.Println("Email:", person.Email)
+	fmt.Println(string(jsonBytes))
 }
