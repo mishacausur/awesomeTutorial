7900002430808c22a3dc4b157e70a1bcdec570d7
Misha Causur <mishacausur@mail.ru>
2023-09-28 22:25:11 +0500

    jsn 1.1

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"strings"
 )
 
-type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
+type Student struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Grade int    `json:"grade"`
 }
 
 func main() {
-	jsonStr := `{"name": "John", "age": 30}`
+	students := []Student{
+		{Name: "Alice", Age: 12, Grade: 7},
+		{Name: "Bob", Age: 13, Grade: 8},
+		{Name: "Charlie", Age: 14, Grade: 9},
+	}
 
-	reader := strings.NewReader(jsonStr)
+	var buf bytes.Buffer
 
-	decoder := json.NewDecoder(reader)
+	encoder := json.NewEncoder(&buf)
 
-	var person Person
-
-	err := decoder.Decode(&person)
+	err := encoder.Encode(students)
 	if err != nil {
-		fmt.Println("Ошибка чтения JSON:", err)
+		fmt.Println("Ошибка при записи JSON-данных:", err)
 		return
 	}
 
-	fmt.Printf("Имя: %s, Возраст: %d\n", person.Name, person.Age)
+	fmt.Println("JSON-данные о студентах:")
+	fmt.Println(buf.String())
 }
